f05b92e031e86458de9ccd364c8f7df85ba9fd77
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-01-26 15:35:26 +0100

    commit 1
    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import importlib
+from kivymd.app import MDApp
+from kivy.core.window import Window
+
+from kaki.app import App
+
+class MeditationAppLive(App, MDApp):
+    KV_FILES = {
+        os.path.join(os.getcwd(), "Screens", "RootScreen", "root_screen.kv"),
+        os.path.join(os.getcwd(), "Screens", "TodayScreen", "today_screen.kv"),
+        os.path.join(os.getcwd(), "Screens", "AllExercisesScreen", "all_exercises_screen.kv"),
+    }
+    CLASSES = {
+        "RootScreen": "Screen.RootScreen.root_screen",
+        "TodayScreen": "Screens.TodayScreen.today_screen",
+        "AllExercisesScreen": "Screens.AllExercisesScreen.all_exercises_screen",
+    }
+    AUTORELOADER_PATHS = [
+        (".", {"recursive": True}),
+    ]
+
+    def build_app(self):
+        import Screens.RootScreen.root_screen
+
+        Window.bind(on_keyboard=self._rebuild)
+        importlib.reload(Screens.RootScreen.root_screen)
+
+        return Screens.RootScreen.root_screen.RootScreen()
+    
+    def _rebuild(self, *args):
+        if args[1] == 32:
+            self._rebuild()
+
+MeditationAppLive().run()
