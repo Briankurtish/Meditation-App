478227ad143830fde7b1cc9c1de4ef30c3f4c675
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-01-27 11:31:50 +0100

    make cards
    here i made the cards on the home screen

---
+++ Screens/AllExercisesScreen/components/card.py
@@ -0,0 +1,4 @@
+from kivymd.uix.card import MDCard
+from kivy.properties import StringProperty
+class Card(MDCard):
+    source = StringProperty()
--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from kivy.core.window import Window
 
 from kaki.app import App
 
-class MeditationAppLive(App):
+class MeditationAppLive(MDApp, App):
     KV_FILES = {
         os.path.join(os.getcwd(), "Screens", "RootScreen", "root_screen.kv"),
         os.path.join(os.getcwd(), "Screens", "TodayScreen", "today_screen.kv"),
         os.path.join(os.getcwd(), "Screens", "AllExercisesScreen", "all_exercises_screen.kv"),
+        os.path.join(os.getcwd(), "Screens", "AllExercisesScreen", "components", "card.kv"),
     }
     CLASSES = {
         "RootScreen": "Screen.RootScreen.root_screen",
         "TodayScreen": "Screens.TodayScreen.today_screen",
         "AllExercisesScreen": "Screens.AllExercisesScreen.all_exercises_screen",
+        "Card": "Screens.AllExercisesScreen.components.card",
     }
     AUTORELOADER_PATHS = [
         (".", {"recursive": True}),
